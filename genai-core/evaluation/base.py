668bcb74c1ed0385138ffbdbd0874b6286452a00
Piyal-Banik <piyalbanik2@gmail.com>
2026-06-02 11:52:22 +0530

    base evaluation class

---
+++ genai-core/evaluation/base.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+class BaseMetric(ABC):
+
+    @abstractmethod
+    def compute(
+        self,
+        predictions,
+        references
+    ):
+        pass
